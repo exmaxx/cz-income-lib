3d44947e3a0183d269594b90131f21d6ea3b5045
Marek Ulicny <xulicny@gmail.com>
2024-07-26 14:33:49 +0200

    refactor: introduce combined rates

--- src/freelancer/grossIncome.ts
+++ src/freelancer/grossIncome.ts
@@ -3,18 +3,23 @@ import { Expenses, Rates } from './freelancer.types'
 function calculateGrossIncome(netIncome: number, expenses: Expenses, rates: Rates): number {
   const { incomeRates, socialRates, healthRates } = rates
 
+  const top = netIncome - incomeRates.nonTaxable * incomeRates.rate - incomeRates.credit
+
+  const ratesCombined =
+    incomeRates.rate +
+    socialRates.basePercentage * socialRates.rate +
+    healthRates.basePercentage * healthRates.rate
+
   let result: number
 
   if ('rate' in expenses) {
-    const top = netIncome - incomeRates.nonTaxable * incomeRates.rate - incomeRates.credit
-
-    const bottom =
-      1 -
-      (1 - expenses.rate) * incomeRates.rate -
-      (1 - expenses.rate) * socialRates.basePercentage * socialRates.rate -
-      (1 - expenses.rate) * healthRates.basePercentage * healthRates.rate
+    const bottom = 1 - (1 - expenses.rate) * ratesCombined
 
     result = Math.floor(top / bottom)
+  } else {
+    const bottom = 1 - ratesCombined
+
+    result = Math.floor(top / bottom + expenses.amount)
   }
 
   return Math.max(result, 0)
